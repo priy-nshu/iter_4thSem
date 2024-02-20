e5ab78f13a6bcd53e8db75414a15011f72d4b9f2
priyanshu dwibedy <priyanshudwibed6@gmail.com>
2024-02-20 13:56:30 +0530

    20/2/24

--- CSW_2/q2.java
+++ CSW_2/q2.java
@@ -1,7 +1,7 @@
 package CSW_2;
 
 public class q2 {
-    public static void main(String[] args) {
-
+    public void sound(){
+        System.out.println("WOOF");
     }
 }
---
+++ CSW_2/q2_2.java
@@ -0,0 +1,12 @@
+package CSW_2;
+
+public class q2_2 extends q2 {
+    public void sound(){
+        System.out.println("WIMP");
+    }
+
+    public static void main(String[] args) {
+        q2 test=new q2_2();
+        test.sound();
+    }
+}
